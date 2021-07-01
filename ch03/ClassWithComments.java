f6d9b07737202dff26fcd2d74915507e71702d6f
Mario Dubreuil <mario_dubreuil@hotmail.com>
2021-07-01 16:38:25 -0400

    comments

---
+++ ch03/ClassWithComments.java
@@ -0,0 +1,17 @@
+/*
+ *	comments that span
+ *	multiple lines
+ */
+
+class MyClass {
+	/*
+		comments that span
+		multiple lines
+	*/
+}
+
+class MyClass2 {
+	String fName;	// first name
+	String id;		// identifier
+	String lName = /* Harry */ "Paul";
+}
